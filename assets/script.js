b83249381f6aa853a90ba49676c44c410f897bef
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-06-29 15:37:18 -0400

    Added buttons for answer choices, added question bank

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,39 @@
 var timeEl = document.querySelector("#timer");
 var secondsLeft = 60;
 
-// Create first card
 var cardHeaderEl = document.getElementById("card-header");
 var cardDescriptEl=document.getElementById("card-description");
-var startBtn = document.querySelector("button");
+var startBtn = document.getElementById("startButton");
 
-cardHeaderEl.innerHTML = "Coding Quiz Challenge";
-cardDescriptEl.innerHTML = "Try to answer the following questions before the time runs out.  Each time you answer incorrectly, the timer will deduct 15 seconds."
-startBtn.innerHTML = "Start Quiz";
+beginningCard();
+startBtn.addEventListener("click", setTimer);
+
+// Create first card
+function beginningCard(){
+    cardHeaderEl.innerHTML = "Coding Quiz Challenge";
+    cardDescriptEl.innerHTML = "Try to answer the following questions before the time runs out.  Each time you answer incorrectly, the timer will deduct 15 seconds."
+    startBtn.innerHTML = "Start Quiz";
+}
+
+// Create Question Cards
+var questionBank = [
+    "What is computational thinking?",
+    "Which of the following is an example of an HTML tag?",
+    "True or False: Classes and IDs are examples of HTML attributes",
+    "What does CSS stand for?",
+    "What is the proper way to identify a class using CSS?",
+    "How do you call a variable in CSS?",
+    "What is the primary purpose of using JavaScript?",
+    "What is the difference between jQuery and vanilla JavaScript?"
+];
+
+function questionCards(){
+    var i = 0;
+    cardHeaderEl.innerHTML = questionBank[i];
+    cardDescriptEl.innerHTML="";
+    startBtn.setAttribute("display", "none");
+}
+
 
 
 // Create timer
