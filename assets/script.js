fd604c812446455c69423968bb90711aae300927
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-06-29 16:57:30 -0400

    Linked start button to functions that allow first question to be shown

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,35 @@ var secondsLeft = 60;
 var cardHeaderEl = document.getElementById("card-header");
 var cardDescriptEl=document.getElementById("card-description");
 var startBtn = document.getElementById("startButton");
+var answerBtn = document.querySelectorAll("#answer-choice")
 
 beginningCard();
-startBtn.addEventListener("click", setTimer);
+startBtn.addEventListener("click", startQuiz);
 
 // Create first card
 function beginningCard(){
     cardHeaderEl.innerHTML = "Coding Quiz Challenge";
     cardDescriptEl.innerHTML = "Try to answer the following questions before the time runs out.  Each time you answer incorrectly, the timer will deduct 15 seconds."
     startBtn.innerHTML = "Start Quiz";
+}
+
+function startQuiz(){
+    startBtn.setAttribute("style", "display:none");
+    // set the timer
+    setTimer();
+    
+    var i=0;
+    while (secondsLeft > 0){
+        //Display question
+        var userAnswer = showQuestion(i);
+        // Check if answer is correct
+        if (userAnswer !== answer){
+            secondsLeft = secondsLeft - 15;
+            i++
+        }
+        i++
+    }
+    
 }
 
 // Create Question Cards
@@ -27,14 +47,27 @@ var questionBank = [
     "What is the difference between jQuery and vanilla JavaScript?"
 ];
 
-function questionCards(){
-    var i = 0;
+var answerBank = [
+    "a) choice 1",
+    "b) choice 2",
+    "c) choice 3",
+    "d) choice 4"
+];
+
+function showQuestion(i){
+    // Display question and answer choices
     cardHeaderEl.innerHTML = questionBank[i];
-    cardDescriptEl.innerHTML="";
-    startBtn.setAttribute("display", "none");
+    cardDescriptEl.innerHTML= ''
+    cardDescriptEl.setAttribute("style", "flex-direction: column");
+    for(j=0; j < answerBtn.length; j++){
+       answerBtn[j].classList.remove("hide");
+       answerBtn[j].innerHTML = answerBank[j]; 
+    }
+    
+
+    // Get answer from user
+    // Return user answer as array at [i]
 }
-
-
 
 // Create timer
 function setTimer(){
